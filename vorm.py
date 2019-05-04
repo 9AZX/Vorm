7d2f0a438e91cffedf324189bd3a2336b2eb4335
Antoine <antoine.tieppo@epitech.eu>
2019-05-04 14:58:47 +0200

    Add(game) addition of the bar in the game view

--- vorm.py
+++ vorm.py
@@ -3,6 +3,10 @@
 import pygame
 import sys
 from options import Option
+
+BG_COLOR = pygame.Color(30, 30, 50)
+HEAT_BAR_IMAGE = pygame.Surface((1000, 10))
+
 class Game:
     def __init__(self):
         self.gameState = 1
@@ -25,21 +29,49 @@ class Game:
     def handlerMenu(self):
         self.setBackground(0, 0)
         pygame.event.pump()
-        for option in self.options:
-            if option.rect.collidepoint(pygame.mouse.get_pos()):
-                option.is_hovered = True
-            else:
-                option.is_hovered = False
-            option.draw(self.gameDisplay, self.menuFont)
-        if (pygame.mouse.get_pressed()[0] and options[0].rect.collidepoint(pygame.mouse.get_pos())):
-            game_ft()
-        if (pygame.mouse.get_pressed()[0] and options[1].rect.collidepoint(pygame.mouse.get_pos())):
-            optionMenu(self.options[0])
-        if (pygame.mouse.get_pressed()[0] and options[2].rect.collidepoint(pygame.mouse.get_pos())):
-            sys.exit(0)
+        self.gameHandler()
+        # for option in self.options:
+        #     if option.rect.collidepoint(pygame.mouse.get_pos()):
+        #         option.is_hovered = True
+        #     else:
+        #         option.is_hovered = False
+        #     option.draw(self.gameDisplay, self.menuFont)
+        # if (pygame.mouse.get_pressed()[0] and options[0].rect.collidepoint(pygame.mouse.get_pos())):
+        #     game_ft()
+        # if (pygame.mouse.get_pressed()[0] and options[1].rect.collidepoint(pygame.mouse.get_pos())):
+        #     optionMenu(self.options[0])
+        # if (pygame.mouse.get_pressed()[0] and options[2].rect.collidepoint(pygame.mouse.get_pos())):
+        #     sys.exit(0)
 
-    def gameHandler():
-        print("Game\n")
+    def bar(self):
+        color = pygame.Color('#f22b2b')
+        for x in range(HEAT_BAR_IMAGE.get_width()):
+            for y in range(HEAT_BAR_IMAGE.get_height()):
+                HEAT_BAR_IMAGE.set_at((x, y), color)
+            if x > 200:
+                color = pygame.Color('#f2b11a')
+            if x > 400:
+                color = pygame.Color('#e0e00f')
+            if x > 600:
+                color = pygame.Color('#2dc611')
+
+    def gameHandler(self):
+        clock = pygame.time.Clock()
+        heat_rect = HEAT_BAR_IMAGE.get_rect(topleft=(100, 400))
+        heat = 100
+        done = False
+
+        while not done:
+            self.bar()
+            heat -= 0.5
+            heat = max(1, min(heat, 100))
+            self.gameDisplay.fill(BG_COLOR)
+            self.gameDisplay.blit(HEAT_BAR_IMAGE, heat_rect, (0, 0, heat_rect.w/100*heat, heat_rect.h))
+            if (heat_rect.w/100*heat) == 10:
+                done = True
+            pygame.display.flip()
+            clock.tick(30)
+
 
     def optionHandler():
         exit = False
